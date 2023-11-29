9edf016aaa235d4aa019694f90365a9cdab0c770
erikvella <vella.erik1@gmail.com>
2023-11-29 14:55:43 +0100

    [init] dashboard

---
+++ resources/views/admin/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header class="bg-dark">
+    <nav class="navbar navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand">Vai al sito</a>
+            <form class="d-flex" role="search">
+
+                <button class="btn btn-light" type="submit"><i class="fa-solid fa-right-from-bracket"></i></button>
+            </form>
+        </div>
+    </nav>
+</header>
